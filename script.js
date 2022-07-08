452af303d89b245d920d829b1b685bc19c339878
goghulpd <94519918+goghulpd@users.noreply.github.com>
2022-07-08 12:34:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll=function()
+{myfunction()};
+
+var navbar=document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myfunction() {
+    if (window.pageYOffset >= sticky)
+    {
+        navbar.classList.add("sticky")
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
